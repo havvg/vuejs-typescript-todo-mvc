db6ba8ad20582ae3e283ee19fd95a147f804d839
Toni Uebernickel <tuebernickel@gmail.com>
2017-10-28 14:56:15 +0200

    use vue-class-component
    $ npm install vue-class-component --save-dev

--- src/App.vue
+++ src/App.vue
@@ -21,43 +21,45 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Component from 'vue-class-component';
 
-export default Vue.extend({
-    name: 'app',
-
+@Component({
+    name: 'app'
+})
+export default class App extends Vue {
     data () {
         return {
             msg: 'Welcome to Your Vue.js App, supporting TypeScript'
         }
     }
-})
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
 h1, h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
